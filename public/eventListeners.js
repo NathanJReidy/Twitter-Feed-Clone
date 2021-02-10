617fa2c759cc03d5d9e6398059ee2d6ec32921e4
Nathan Reidy <nathanjreidy@gmail.com>
2021-02-10 17:52:56 +1100

    Fixed status update text area box so that it auto expands upon writing

--- public/eventListeners.js
+++ public/eventListeners.js
@@ -6,9 +6,14 @@ let mainTweetBtn = document.querySelector("#mainTweetBtn");
 
 
 textarea.addEventListener("keyup", () => {
-    statusWrapper.className = "flex h-40 space-x-6 items-center";
-    textarea.className = "w-full mt-4 h-full flex flex-wrap break-words text-gray-500 text-sm placeholder-gray-500 focus:outline-none";
-    mainTweetBtn.className = "flex text-white py-2 px-4 bg-blue-500 cursor-default rounded-full hover:bg-blue-600 cursor:pointer";
+    textarea.style = `display: flex; height: ${textarea.scrollHeight}px`;
+    console.log(textarea.scrollHeight);
+
+    // statusWrapper.className = "flex h-40 space-x-6 items-center";
+    // textarea.className = "w-full mt-4 h-full flex flex-wrap break-words text-gray-500 text-sm placeholder-gray-500 focus:outline-none";
+    // mainTweetBtn.className = "flex text-white py-2 px-4 bg-blue-500 cursor-default rounded-full hover:bg-blue-600 cursor:pointer";
+
+
 });
 
 
