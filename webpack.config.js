94a5d31fa575e55b395b7992e5389b6b275911f1
Nathan Reidy <nathanjreidy@gmail.com>
2021-02-26 00:56:31 +1100

    Added babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,24 @@ const path = require('path');
 
 module.exports = {
   entry: ['./src/DOMchanges.js', './src/DOMmain.js', './src/eventListeners.js', './src/logic.js'],
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+    ]
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
 };
+
